2ae65677042f32f2ff49eb2ef1a009a655762b16
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-12-29 12:45:07 +0100

    first commit

---
+++ src/main/java/tn/esprit/examenclinique2022/ExamenClinique2022Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.examenclinique2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenClinique2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenClinique2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/examenclinique2022/ExamenClinique2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.examenclinique2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenClinique2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
